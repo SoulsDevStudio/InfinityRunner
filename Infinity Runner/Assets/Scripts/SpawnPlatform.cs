a0ce7fb8486a46ad8d8a7179b4d80c0efa770f93
Matheus Ferreira <soulsdevstudio@gmail.com>
2022-08-02 16:07:53 -0300

    Recycling the Platforms

--- Infinity Runner/Assets/Scripts/SpawnPlatform.cs
+++ Infinity Runner/Assets/Scripts/SpawnPlatform.cs
@@ -6,28 +6,56 @@ public class SpawnPlatform : MonoBehaviour
 {
 
     public List<GameObject> platforms = new List<GameObject>();
-
+    
     public float distance;
-
     public float offset;
+
+    Transform player;
+    Transform currentPlaformPoint;
+    int platformIndex;
+
+    List<Transform> currentPlatforms = new List<Transform>();
 
     void Start()
     {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+
         for(int i =0; i < platforms.Count; i++)
         {
-            Instantiate(platforms[i], new Vector2(distance* i,0),transform.rotation);
-            offset += 30f;
+            Transform p = Instantiate(platforms[i], new Vector2(distance* i,0),transform.rotation).transform;
+            currentPlatforms.Add(p);
+            offset += distance;
         }
+
+        currentPlaformPoint = currentPlatforms[platformIndex].GetComponent<Platform>().finalPoint;
     }
 
     void Update()
     {
-        
+        Move();
+    }
+
+    void Move()
+    {
+        float distance = player.position.x - currentPlaformPoint.position.x;
+
+        if(distance >= 1)
+        {
+            Recycle(currentPlatforms[platformIndex].gameObject);
+            platformIndex++;
+
+            if(platformIndex > currentPlatforms.Count - 1)
+            {
+                platformIndex = 0;
+            }
+
+            currentPlaformPoint = currentPlatforms[platformIndex].GetComponent<Platform>().finalPoint;
+        }
     }
 
     public void Recycle(GameObject platform)
     {
         platform.transform.position = new Vector2(offset, 0);
-        offset += 30f;
+        offset += distance;
     }
 }
