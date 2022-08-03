5e3146161b7d85c20729ba466c2437bcb66d0486
Matheus Ferreira <soulsdevstudio@gmail.com>
2022-08-03 13:38:45 -0300

    Player - Animation

--- Infinity Runner/Assets/Scripts/Player.cs
+++ Infinity Runner/Assets/Scripts/Player.cs
@@ -5,21 +5,26 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     Rigidbody2D rig;
+    bool isJumping;
+
+    public Animator anim;
 
     public float speed;
     public float jumpForce;
 
     void Start()
     {
+
         rig = GetComponent<Rigidbody2D>();
     }
 
     
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && isJumping)
         {
             rig.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            anim.SetBool("isJumping", true);
         }
     }
 
@@ -27,4 +32,23 @@ public class Player : MonoBehaviour
     {
         rig.velocity = new Vector2(speed, rig.velocity.y);
     }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.layer == 3)
+        {
+            isJumping = true;
+            anim.SetBool("isJumping", false);
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        if(collision.gameObject.layer == 3)
+        {
+            isJumping = false;
+        }
+    }
+
+
 }
