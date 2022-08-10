9fc9712f7ef0bfbc93e9e208c6b16fa79c087ed6
Matheus Ferreira <soulsdevstudio@gmail.com>
2022-08-10 17:36:36 -0300

    Enemy One

---
+++ Infinity Runner/Assets/Scripts/FlyEnemy.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyEnemy : MonoBehaviour
+{
+
+    Rigidbody2D rig;
+    public float speed;
+
+    void Start()
+    {
+        rig = GetComponent<Rigidbody2D>();
+        Destroy(gameObject, 5f);
+    }
+
+    void FixedUpdate()
+    {
+        rig.velocity = Vector2.left * speed;
+    }
+}
---
+++ Infinity Runner/Assets/Scripts/SpawnEnemies.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnEnemies : MonoBehaviour
+{
+
+    public List<GameObject> EnemiesList = new List<GameObject>();
+    public float timeCount;
+    public float spawnTimer;
+
+    
+    void Update()
+    {
+        timeCount += Time.deltaTime;
+        if(timeCount >= spawnTimer)
+        {
+            SpawnEnemy();
+            timeCount = 0;
+        }
+    }
+
+    void SpawnEnemy()
+    {
+        Instantiate(EnemiesList[0], transform.position + new Vector3(0, Random.Range(0f,2.5f),0), transform.rotation);
+    }
+}
