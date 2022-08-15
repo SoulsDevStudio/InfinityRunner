7d30430119ab1a606ef6ed5d856ba6dd20a63799
Matheus Ferreira <soulsdevstudio@gmail.com>
2022-08-15 16:46:27 -0300

    Enemy Shooter

---
+++ Infinity Runner/Assets/Scripts/Bomb.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bomb : MonoBehaviour
+{
+    Rigidbody2D rig;
+
+    public float x;
+    public float y;
+
+    void Start()
+    {
+        rig = GetComponent<Rigidbody2D>();
+        rig.AddForce(new Vector2(x, y),ForceMode2D.Impulse);
+
+        Destroy(gameObject, 5f);
+    }
+
+}
---
+++ Infinity Runner/Assets/Scripts/EnemyBomb.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBomb : MonoBehaviour
+{
+    public GameObject bomb;
+    public Transform pointFire;
+
+    float throwTimer;
+    public float throwCount;
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        throwTimer += Time.deltaTime;
+        
+        if(throwTimer >= throwCount)
+        {
+            Instantiate(bomb, pointFire.position, pointFire.rotation);
+            throwTimer = 0;
+        }
+    }
+}
